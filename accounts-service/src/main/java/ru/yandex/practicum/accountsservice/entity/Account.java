73dad8eb5a4ebfd2ca201f014cf1be7ab26f0e3f
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-02-03 00:42:16 +0300

    Added new field login

--- accounts-service/src/main/java/ru/yandex/practicum/accountsservice/entity/Account.java
+++ accounts-service/src/main/java/ru/yandex/practicum/accountsservice/entity/Account.java
@@ -18,6 +18,9 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "login", unique = true)
+    private String login;
+
     @Column(name = "name")
     private String name;
 
