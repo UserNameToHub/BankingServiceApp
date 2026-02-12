605b1472b764c5e27d2ea263192b75fed38af408
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-02-12 21:39:53 +0300

    Added filter for token

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/JwtTokenRelayFilter.java
@@ -0,0 +1,29 @@
+package com.example.gatewayservice.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtTokenRelayFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("Starting to extract token from request");
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String authHeader = httpRequest.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwtToken = authHeader.substring("Bearer ".length());
+            httpRequest.setAttribute("Authorization", "Bearer " + jwtToken);
+        }
+
+        log.debug("Sending the token  from proxy further along the route");
+        chain.doFilter(request, response);
+    }
+}
