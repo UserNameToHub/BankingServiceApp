6e5b24dd3fd321fc8f52de3d0852a48d372c9572
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-02-15 22:23:35 +0300

    Added exception

---
+++ accounts-service/src/main/java/ru/yandex/practicum/accountsservice/exception/NoAccountException.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.accountsservice.exception;
+
+public class NoAccountException extends RuntimeException {
+    private String message;
+
+    public NoAccountException(String message) {
+        super(message);
+    }
+}
