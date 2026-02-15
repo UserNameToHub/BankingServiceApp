ae3a287e96577621907d58762960075d2c6250df
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-02-15 22:25:06 +0300

    Change TransferService

--- transfer-service/src/main/java/ru/yandex/ptracticum/transferservice/service/TransferService.java
+++ transfer-service/src/main/java/ru/yandex/ptracticum/transferservice/service/TransferService.java
@@ -2,6 +2,7 @@ package ru.yandex.ptracticum.transferservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.ptracticum.transferservice.client.AccountClient;
 import ru.yandex.ptracticum.transferservice.dto.TransferDto;
@@ -12,8 +13,11 @@ import ru.yandex.ptracticum.transferservice.dto.TransferDto;
 public class TransferService {
     private final AccountClient client;
 
-    public String transfer(TransferDto transferDto) {
-        log.info("A transfer request for user {}, amount {}", transferDto.login(), transferDto.value());
+    public String transfer(int value, String login) {
+        String owner = SecurityContextHolder.getContext().getAuthentication().getName();
+        TransferDto transferDto = new TransferDto(value, owner, login);
+        log.info("A transfer request from {} to {}, amount {}",
+                transferDto.fromLogin(), transferDto.toLogin(), transferDto.value());
         String result = client.transfer(transferDto);
         log.info("Successful transfer: {}", result);
         return result;
