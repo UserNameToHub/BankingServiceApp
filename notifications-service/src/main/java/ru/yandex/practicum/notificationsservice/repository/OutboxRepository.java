60575344c356f5c4d0ff8f44f2e0432e3e82f90a
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-02-15 17:41:02 +0300

    Added a finding method by microservice enum

--- notifications-service/src/main/java/ru/yandex/practicum/notificationsservice/repository/OutboxRepository.java
+++ notifications-service/src/main/java/ru/yandex/practicum/notificationsservice/repository/OutboxRepository.java
@@ -1,9 +1,13 @@
 package ru.yandex.practicum.notificationsservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.notificationsservice.dto.Microservice;
 import ru.yandex.practicum.notificationsservice.entity.CreateOutbox;
 
 @Repository
 public interface OutboxRepository extends CrudRepository<CreateOutbox, Long> {
+    Page<CreateOutbox> findAllByMicroservice(Microservice microSrv, Pageable page);
 }
