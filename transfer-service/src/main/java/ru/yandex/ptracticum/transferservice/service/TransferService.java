89229bf78b7b809029c01ce66de8d3749197be1b
meinepostkam@gmail.com <meinepostkam@gmail.com>
2026-03-06 04:33:16 +0300

    Added Kafka service to transfer service and implemented message sending to Notification service

--- transfer-service/src/main/java/ru/yandex/ptracticum/transferservice/service/TransferService.java
+++ transfer-service/src/main/java/ru/yandex/ptracticum/transferservice/service/TransferService.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Component;
 import ru.yandex.ptracticum.transferservice.client.TransferClient;
 import ru.yandex.ptracticum.transferservice.dto.TransferDto;
 import ru.yandex.ptracticum.transferservice.dto.TransferResponse;
+import ru.yandex.ptracticum.transferservice.kafka.MessageProducer;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class TransferService {
     private final TransferClient client;
+    private final MessageProducer producer;
 
     public TransferResponse transfer(int value, String login) {
         String owner = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -20,6 +22,7 @@ public class TransferService {
         log.info("A transfer request from {} to {}, amount {}",
                 transferDto.fromLogin(), transferDto.toLogin(), transferDto.value());
         TransferResponse result = client.transfer(transferDto);
+        producer.sendMessage(transferDto);
         log.info("Successful transfer: {}", result);
         return result;
     }
